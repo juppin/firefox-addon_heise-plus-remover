4dd9f7f78838d11f0194acf1dc80b4eda32963ef
Johann Neuhauser <johann@it-neuhauser.de>
2019-09-17 20:56:46 +0200

    Implement proof of concept

--- remove_content.js
+++ remove_content.js
@@ -1,4 +1,33 @@
-/*
-Just draw a border round the document.body.
-*/
-document.body.style.border = "5px solid red";
+
+function removeArticleWhichContainsElementWithClassName(names) {
+    var elements = document.getElementsByClassName(names);
+    console.log(elements);
+    //[].forEach.call(elements, function (element) {
+    for (var element of elements) {
+        console.log("found " + element.localName + " with class name: " + names);
+        currentElement = element;
+        while (currentElement.parentElement != null) {
+            currentElement = currentElement.parentElement;
+            console.log(currentElement.tagName);
+            if (currentElement.tagName === 'ARTICLE') {
+                console.log("removing: " + currentElement.tagName);
+                currentElement.remove();
+                break;
+            }
+        }
+    }
+    //});
+}
+
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function() {
+    var classNames = 'heiseplus-logo-small'
+    removeArticleWhichContainsElementWithClassName(classNames);
+});
